db6d45f5e9267c21379885307da9d79db48e763f
Vivek Jain <jain.vivek.anand@gmail.com>
2019-12-01 16:30:59 -0800

    Add eclipse project files

--- src/Constant.java
+++ src/Constant.java
@@ -2,5 +2,5 @@ public class Constant {
 	static String INPUT_PATH = "/home/vivek/workspace/CS201Fall19/Analysis/";
 	private static String OUTPUT_PATH = "/home/vivek/workspace/CS201Fall19/sootOutput/";
 	static String ARGS = "-allow-phantom-refs -process-dir " + OUTPUT_PATH;
-	static String CLASS_UNDER_ANALYSIS = "Test1";
+	static String CLASS_UNDER_ANALYSIS = "Test4";
 }
